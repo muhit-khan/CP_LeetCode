f5fa15e07cbd9c34fa2e66f94505b32f8af2ae86
Muhit Khan <muhitkhan_bd@outlook.com>
2023-08-05 09:26:51 +0600

    Create 121.best-time-to-buy-and-sell-stock.java

---
+++ Java/121.best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,25 @@
+/*
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int maxProfit = 0;
+        int minPrice = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+}
+
