fb8bf81965ad72ba287406aeef4cbca8ac40bf91
Muhit Khan <muhitkhan_bd@outlook.com>
2023-08-22 23:27:41 +0530

    Create 20.valid-parentheses.java

---
+++ Java/20.valid-parentheses.java
@@ -0,0 +1,27 @@
+/*
+ * [20] Valid Parentheses
+ */
+
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if ((s.charAt(i) == ')' && top == '(') || (s.charAt(i) == '}' && top == '{') || (s.charAt(i) == ']' && top == '[')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
