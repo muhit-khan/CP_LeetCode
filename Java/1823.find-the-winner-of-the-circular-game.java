51cb442d1105501b940983d768f8b3a45deffec3
Muhit Khan <muhitkhan_bd@outlook.com>
2023-08-26 23:24:59 +0530

    Create 1823.find-the-winner-of-the-circular-game.java

---
+++ Java/1823.find-the-winner-of-the-circular-game.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1823 lang=java
+ *
+ * [1823] Find the Winner of the Circular Game
+ */
+
+// @lc code=start
+class Solution {
+    public int findTheWinner(int n, int k) {
+        
+    }
+}
+// @lc code=end
+
